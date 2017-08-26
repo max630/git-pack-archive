016b9aa6c5c82aba3a778ed0ba93a1ed853ebb01
Max Kirillov <max@max630.net>
2017-08-26 17:42:03 +0300

    init

---
+++ git-pack-archive/Program.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace gitpackarchive {
+    class MainClass {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
