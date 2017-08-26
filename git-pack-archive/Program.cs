47d4391be7218ac6aa665b041198449f3d98d194
Max Kirillov <max@max630.net>
2017-08-26 18:03:39 +0300

    busy document

--- git-pack-archive/Program.cs
+++ git-pack-archive/Program.cs
@@ -4,7 +4,24 @@ namespace gitpackarchive {
     class MainClass {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ReportBusy();
         }
+
+        private static void ReportBusy()
+        {
+            Console.WriteLine("Status: 503 Lunch");
+            Console.WriteLine("Retry-after: 60");
+            Console.WriteLine("Content-type: text/html");
+            Console.WriteLine("");
+            Console.WriteLine(busyDoc);
+        }
+    
+        private static readonly string busyDoc =
+@"<html>
+<title>Bisy</title>
+<body>
+Somebody is already dowloading. Please try again later.
+</body>
+</html>";
     }
 }
